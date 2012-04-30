90dfe78edef8106b1d84763ac871cd96f890760a
Christopher Bertels <bakkdoor@flasht.de>
2012-04-30 15:04:47 +0200

    alten code entfernt

--- blatt01/src/aufgabe1/mystrings.h
+++
@@ -1,9 +0,0 @@
- 
-#ifndef MYSTRINGS_H
-#define MYSTRINGS_H
-
-int strcmp(const char *str1, const char *str2);
-char *strcpy(char *destination, const char *source);
-size_t strlen(const char *str);
-
-#endif // MYSTRINGS_H
