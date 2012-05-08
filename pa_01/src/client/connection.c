bcf3976aba12117df3e7a1b9df8adaca2fcdad4d
Christopher Bertels <bakkdoor@flasht.de>
2012-05-08 20:47:25 +0200

    fix formatting

--- pa_01/src/client/connection.c
+++ pa_01/src/client/connection.c
@@ -31,23 +31,42 @@ int connection_send_client_message(int sock, client_message_t * msg)
   case CL_CON_REQ:
     len = 1 + 4 + strlen(msg->cl_con_req.name) + 1;
     buf = calloc(1, len);
-    sprintf(buf, "%1d%2d%s", msg->type, htons(strlen(msg->cl_con_req.name)), msg->cl_con_req.name);
+    sprintf(buf,
+            "%1d%2d%s",
+            msg->type,
+            htons(strlen(msg->cl_con_req.name)),
+            msg->cl_con_req.name);
     break;
+
   case CL_ROOM_MSG:
     len = 1 + 4 + strlen(msg->cl_room_msg.room_name) + 1 + 1;
     buf = calloc(1, len);
-    sprintf(buf, "%1d%2d%s%1d", msg->type, htons(strlen(msg->cl_room_msg.room_name)), msg->cl_room_msg.room_name, msg->cl_room_msg.action);
+    sprintf(buf,
+            "%1d%2d%s%1d",
+            msg->type,
+            htons(strlen(msg->cl_room_msg.room_name)),
+            msg->cl_room_msg.room_name,
+            msg->cl_room_msg.action);
     break;
+
   case CL_MSG:
     len = 1 + 4 + strlen(msg->cl_msg.room_name) + 1 + strlen(msg->cl_msg.message) + 1;
     buf = calloc(1, len);
-    sprintf(buf, "%1d%2d%s%2d%s", msg->type, htons(strlen(msg->cl_msg.room_name)), msg->cl_msg.room_name, htons(strlen(msg->cl_msg.room_name)), msg->cl_msg.message);
+    sprintf(buf,
+            "%1d%2d%s%2d%s",
+            msg->type,
+            htons(strlen(msg->cl_msg.room_name)),
+            msg->cl_msg.room_name,
+            htons(strlen(msg->cl_msg.room_name)),
+            msg->cl_msg.message);
     break;
+
   case CL_DISC_REQ:
     len = 1;
     buf = calloc(1, len);
     sprintf(buf, "%1d", msg->type);
     break;
+
   default:
     fprintf(stderr, "Unknown message type: %d", msg->type);
     assert(0);
