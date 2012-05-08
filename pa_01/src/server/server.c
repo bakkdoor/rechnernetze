f4f369885c7ed05446d9d66a801899c52d3d048f
Christopher Bertels <bakkdoor@flasht.de>
2012-05-08 20:59:24 +0200

    TODO comment

--- pa_01/src/server/server.c
+++ pa_01/src/server/server.c
@@ -51,6 +51,8 @@ int main(int argc, char ** argv)
   Running = true;
 
   while(Running) {
+    /* TODO:
+       connection_handle() spaws thread for each connection */
     connection_handle(connection_accept(ServerSock));
   }
 
