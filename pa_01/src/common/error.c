ba2a903130b2ee7481beb4e0e16282bfd3547eab
Christopher Bertels <bakkdoor@flasht.de>
2012-05-10 17:51:28 +0200

    prefix error messages

--- pa_01/src/common/error.c
+++ pa_01/src/common/error.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "error.h"
 
 void error(char * message, bool quit)
 {
-  fprintf(stderr, "%s\n", message);
+  fprintf(stderr, "ERROR: %s\n", message);
   if(quit)
     exit(1);
 }
