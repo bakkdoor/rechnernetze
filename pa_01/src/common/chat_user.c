9b5842ab1efcfa323616e549fffb1c5d3d85a023
Christopher Bertels <bakkdoor@flasht.de>
2012-05-11 14:46:40 +0200

    made chat_user struct private

--- pa_01/src/common/chat_user.c
+++ pa_01/src/common/chat_user.c
@@ -3,13 +3,17 @@
 #include "chat_user.h"
 #include "chat_room.h"
 
-chat_user_t * chat_user_new(const char * name, int sockfd)
+struct chat_user {
+  char * name;
+  list_t * rooms;
+};
+
+chat_user_t * chat_user_new(const char * name)
 {
   chat_user_t * user = calloc(1, sizeof(chat_user_t));
   if(!user)
     return NULL;
 
-  user->sockfd = sockfd;
   user->rooms = list_new();
   user->name = calloc(strlen(name) + 1, sizeof(char));
   strcpy(user->name, name);
@@ -27,4 +31,20 @@ void chat_user_delete(void * _user)
   free(user->name);
   list_delete(user->rooms, chat_room_delete);
   free(user);
+}
+
+list_t * chat_user_rooms(chat_user_t * user)
+{
+  if(!user)
+    return NULL;
+
+  return user->rooms;
+}
+
+char * chat_user_name(chat_user_t * user)
+{
+  if(!user)
+    return NULL;
+
+  return user->name;
 }
--- pa_01/src/common/chat_user.h
+++ pa_01/src/common/chat_user.h
@@ -3,13 +3,11 @@
 
 #include "list.h"
 
-typedef struct chat_user {
-  char * name;
-  list_t * rooms;
-  int sockfd;
-} chat_user_t;
+typedef struct chat_user chat_user_t;
 
-chat_user_t * chat_user_new(const char * name, int sockfd);
+chat_user_t * chat_user_new(const char * name);
 void chat_user_delete(void * user);
+list_t * chat_user_rooms(chat_user_t * user);
+char * chat_user_name(chat_user_t * user);
 
 #endif /* _CHAT_USER_H_ */
