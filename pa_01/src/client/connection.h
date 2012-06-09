e7b1a858a5cb74837c5d15a7f15a496b163a28f4
Waldemar Smirnow <wsmirnow@uni-osnabrueck.de>
2012-06-09 19:50:27 +0200

    client docs

--- pa_01/src/client/connection.h
+++ pa_01/src/client/connection.h
@@ -5,18 +5,32 @@
 
 #include "../common/messages.h"
 
+/**
+ Client connection struct. Holds Server network information (socket, hostname,...) and username.
+ */
 typedef struct client_connection client_connection_t;
 
+/**
+ * Start connection (setup socket, start request to communicate as user with given name).
+ * @param server_hostname server hostname or ip address
+ * @param server_port server port
+ * @param username user name
+ * @return sonnection struct or NULL if can not communicate with server or username is allready in use
+ */
 client_connection_t * connection_setup(const char * server_hostname, const char * server_port, const char * username);
-void connection_close(client_connection_t * cli_conn);
-void connection_delete(client_connection_t * cli_conn);
-int connection_send_client_message(client_connection_t * cli_conn, client_message_t * msg);
-server_message_t * connection_recv_client_message(client_connection_t * cli_conn);
-int connection_has_incoming_data(client_connection_t * cli_conn, int timeout_sec);
 
+/**
+ * Parse given buffer and creates a client message struct for this.
+ * @return client message struct or NULL if fail (wrong input,...)
+ */
 client_message_t * parse_client_message(const char * buf);
+
+/**
+ * Handle input data (typed in from user at stdin or incomming from server).
+ * @param cli_conn connection struct
+ * @param timeout_sec timeout in seconds
+ */
 void connection_handle_socks(client_connection_t * cli_conn, int timeout_sec);
-void handle_server_message(server_message_t * msg);
 
 
 #endif /* _CLIENT_CONNECTION_H_ */
