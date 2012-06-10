212b9e7f723631ca0df488d7123cdf355c9866c0
Christopher Bertels <bakkdoor@flasht.de>
2012-06-10 19:35:40 +0200

    add chat_room documentation

--- pa_01/src/server/chat_room.h
+++ pa_01/src/server/chat_room.h
@@ -10,11 +10,40 @@ typedef struct chat_room {
   list_t * clients;
 } chat_room_t;
 
+/**
+ * @param name Name of new chat room.
+ * @return New chat room or NULL on failure.
+ **/
 chat_room_t * chat_room_new(const char * name);
+
+/**
+ * @param room chat_room to be deleted.
+ **/
 void chat_room_delete(void * room);
+
+/**
+ * @param room chat_room to get client count for.
+ * @return Number of clients in room.
+ **/
 int chat_room_client_count(const chat_room_t * room);
+
+/**
+ * @param room Chat room to add client to.
+ * @param client Client to be added to room.
+ **/
 void chat_room_add_client(chat_room_t * room, struct client * client);
+
+/**
+ * @param room Chat room to remove client from.
+ * @param client Client to be removed from room.
+ **/
 void chat_room_remove_client(chat_room_t * room, struct client * client);
+
+/**
+ * @param room Chat room to check client for.
+ * @param client Client to be checked in room.
+ * @return true if chat room has client, false otherwise.
+ **/
 bool chat_room_has_client(const chat_room_t * room, const struct client * client);
 
 #endif /* _CHAT_ROOM_H_ */
