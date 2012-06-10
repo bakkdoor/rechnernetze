d1af3dc734f350f5d424d67ae70f161c5e6388b6
Christopher Bertels <bakkdoor@flasht.de>
2012-06-10 20:50:04 +0200

    don't show output prefix in client

--- pa_01/src/common/output.h
+++ pa_01/src/common/output.h
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 #include <stdarg.h>
 
+void set_show_output_prefix(bool show_prefix);
 void info(char * message, ...);
 void warn(char * message, ...);
 void error(bool quit, char * message, ...);
