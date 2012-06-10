fd84bb17f2c71aa5e0d12803bdc1a85d6b05e56d
Christopher Bertels <bakkdoor@flasht.de>
2012-06-10 22:26:52 +0200

    add docs to output.h

--- pa_01/src/common/output.c
+++ pa_01/src/common/output.c
@@ -11,35 +11,35 @@ void set_show_output_prefix(bool show_prefix)
   _output_show_prefix = show_prefix;
 }
 
-void info(char * message, ...)
+void info(char * fmt, ...)
 {
   va_list args;
-  va_start(args, message);
+  va_start(args, fmt);
   if(_output_show_prefix)
     fprintf(stdout, "INFO: ");
-  vfprintf(stdout, message, args);
+  vfprintf(stdout, fmt, args);
   fprintf(stdout, "\n");
   va_end(args);
 }
 
-void warn(char * message, ...)
+void warn(char * fmt, ...)
 {
   va_list args;
-  va_start(args, message);
+  va_start(args, fmt);
   if(_output_show_prefix)
     fprintf(stdout, "WARNING: ");
-  vfprintf(stdout, message, args);
+  vfprintf(stdout, fmt, args);
   fprintf(stdout, "\n");
   va_end(args);
 }
 
-void error(bool quit, char * message, ...)
+void error(bool quit, char * fmt, ...)
 {
   va_list args;
-  va_start(args, message);
+  va_start(args, fmt);
   if(_output_show_prefix)
     fprintf(stderr, "ERROR: ");
-  vfprintf(stderr, message, args);
+  vfprintf(stderr, fmt, args);
   fprintf(stderr, "\n");
   va_end(args);
   if(quit)
--- pa_01/src/common/output.h
+++ pa_01/src/common/output.h
@@ -4,9 +4,25 @@
 #include <stdbool.h>
 #include <stdarg.h>
 
+/**
+ * @param show_prefix If false disable prefix output (e.g. "INFO: ") in output functions
+ **/
 void set_show_output_prefix(bool show_prefix);
-void info(char * message, ...);
-void warn(char * message, ...);
-void error(bool quit, char * message, ...);
+
+/**
+ * @param fmt Format string (similar to printf()) - printed to stdout.
+ **/
+void info(char * fmt, ...);
+
+/**
+ * @param fmt Format string (similar to printf()) - printed to stdout.
+ **/
+void warn(char * fmt, ...);
+
+/**
+ * @param quit Quits the current process (via exit(1)) if true.
+ * @param fmt Format string (similar to printf()) - printed to stderr.
+ **/
+void error(bool quit, char * fmt, ...);
 
 #endif /* _ERROR_H_ */
