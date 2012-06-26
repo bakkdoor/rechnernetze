4f810c4b739d45eb8adb6b75cb87cb504ae7d776
Waldemar Smirnow <wsmirnow@uni-osnabrueck.de>
2012-06-26 19:21:27 +0200

    simple echo application created to test route between server <=> client
    Signed-off-by: Waldemar Smirnow <wsmirnow@uni-osnabrueck.de>

--- pa_02/src/simulator/simulation.cpp
+++ pa_02/src/simulator/simulation.cpp
@@ -10,6 +10,14 @@
 #include "ns3/applications-module.h"
 #include "ns3/ipv4-global-routing-helper.h"
 
+
+// Networg Topology
+//
+//             10.0.0.0/8                192.168.178.0/24
+// Server --------------------- Router -------------------- Client
+//    ( 2 Mbps, 20 ms, Drop Tail )   ( 2 Mbps, 20 ms, Drop Tail )
+//
+
 using namespace ns3;
 
 NS_LOG_COMPONENT_DEFINE ("rene-pa_02");
@@ -45,7 +53,7 @@ main (int argc, char *argv[])
   
   // assign ip adresses
   Ipv4AddressHelper adressHelper;
-  adressHelper.SetBase("10.1.1.0", "255.255.255.0");
+  adressHelper.SetBase("10.0.0.0", "255.0.0.0");
   Ipv4InterfaceContainer serverRouterInterfaces;
   serverRouterInterfaces = adressHelper.Assign(serverRouterDevices);
   
@@ -53,7 +61,30 @@ main (int argc, char *argv[])
   Ipv4InterfaceContainer routerClinetInterfaces;
   routerClinetInterfaces = adressHelper.Assign(routerClientDevices);
   
+  // create routing tables
+  Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
   
+  ///////////////////////////////////////////////////////////////////////////
+  
+  // test with ping application
+  // client ping server 5 times with one second intervall 
+  LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
+  LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
+  
+  UdpEchoServerHelper echoServer (9);
+
+  ApplicationContainer serverApps = echoServer.Install (serverRouterNodes.Get (0));
+  serverApps.Start (Seconds (1.0));
+  serverApps.Stop (Seconds (10.0));
+  
+  UdpEchoClientHelper echoClient (serverRouterInterfaces.GetAddress (0), 9);
+  echoClient.SetAttribute ("MaxPackets", UintegerValue (5));
+  echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
+  echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
+  
+  ApplicationContainer clientApps = echoClient.Install (routerClientNodes.Get (1));
+  clientApps.Start (Seconds (2.0));
+  clientApps.Stop (Seconds (10.0));
   
   
   Simulator::Run ();
