be72553b0702c0bc5da7cf9dd2b2596e88c50d20
Waldemar Smirnow <wsmirnow@uni-osnabrueck.de>
2012-06-27 20:09:48 +0200

    simulate tcp sender and reciever, fith tutorial exmaple added

--- pa_02/src/simulator/simulation.cpp
+++ pa_02/src/simulator/simulation.cpp
@@ -24,7 +24,7 @@ NS_LOG_COMPONENT_DEFINE ("rene-pa_02");
 
 int 
 main (int argc, char *argv[])
-{
+{  
   // create nodes
   NodeContainer serverRouterNodes;
   serverRouterNodes.Create(2);
@@ -34,8 +34,12 @@ main (int argc, char *argv[])
   routerClientNodes.Create(1);
   
   // create devices and set settings
-  PointToPointHelper deviceSettingsHelper;
-  deviceSettingsHelper.SetDeviceAttribute("DataRate", StringValue("2Mbps"));
+  CsmaHelper deviceSettingsHelper;
+  deviceSettingsHelper.SetChannelAttribute("DataRate", StringValue("2Mbps"));
+  
+//  PointToPointHelper deviceSettingsHelper;
+//  deviceSettingsHelper.SetDeviceAttribute("DataRate", StringValue("2Mbps"));
+  
   deviceSettingsHelper.SetChannelAttribute("Delay", StringValue("20ms"));
   deviceSettingsHelper.SetQueue(DropTailQueue::GetTypeId().GetName());
   
@@ -64,28 +68,49 @@ main (int argc, char *argv[])
   // create routing tables
   Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
   
+  // create pcap files for each device
+  deviceSettingsHelper.EnablePcapAll("trace");
+  
   ///////////////////////////////////////////////////////////////////////////
   
   // test with ping application
   // client ping server 5 times with one second intervall 
-  LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
-  LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
+//  LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
+//  LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
+//  
+//  UdpEchoServerHelper echoServer (9);
+//
+//  ApplicationContainer serverApps = echoServer.Install (serverRouterNodes.Get (0));
+//  serverApps.Start (Seconds (1.0));
+//  serverApps.Stop (Seconds (10.0));
+//  
+//  UdpEchoClientHelper echoClient (serverRouterInterfaces.GetAddress (0), 9);
+//  echoClient.SetAttribute ("MaxPackets", UintegerValue (5));
+//  echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
+//  echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
+//  
+//  ApplicationContainer clientApps = echoClient.Install (routerClientNodes.Get (1));
+//  clientApps.Start (Seconds (2.0));
+//  clientApps.Stop (Seconds (10.0));
+//  
   
-  UdpEchoServerHelper echoServer (9);
-
-  ApplicationContainer serverApps = echoServer.Install (serverRouterNodes.Get (0));
-  serverApps.Start (Seconds (1.0));
-  serverApps.Stop (Seconds (10.0));
+  ////////////////////
   
-  UdpEchoClientHelper echoClient (serverRouterInterfaces.GetAddress (0), 9);
-  echoClient.SetAttribute ("MaxPackets", UintegerValue (5));
-  echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
-  echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
+  LogComponentEnable("PacketSink", LOG_LEVEL_INFO);
   
-  ApplicationContainer clientApps = echoClient.Install (routerClientNodes.Get (1));
-  clientApps.Start (Seconds (2.0));
-  clientApps.Stop (Seconds (10.0));
+  uint16_t serverPort = 8080;
+  PacketSinkHelper packetSinkHelper ("ns3::TcpSocketFactory", InetSocketAddress (Ipv4Address::GetAny (), serverPort));
+  ApplicationContainer sinkApps = packetSinkHelper.Install (serverRouterNodes.Get (0));
+  sinkApps.Start (Seconds (1.0));
+  sinkApps.Stop (Seconds (10.0));
   
+  LogComponentEnable("BulkSendApplication", LOG_LEVEL_LOGIC);
+  
+  Address serverAddress (InetSocketAddress (serverRouterInterfaces.GetAddress (0), serverPort));
+  BulkSendHelper packetSendHelper ("ns3::TcpSocketFactory", serverAddress);
+  ApplicationContainer senderApps = packetSendHelper.Install(routerClientNodes.Get(1));
+  senderApps.Start(Seconds(2.0));
+  senderApps.Stop(Seconds(10.0));
   
   Simulator::Run ();
   Simulator::Destroy ();
